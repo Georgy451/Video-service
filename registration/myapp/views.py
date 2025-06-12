38ea3e905779d70e89b7c60a920fdb287ffe8de0
Georgy451 <wiktory1975@gmail.com>
2025-06-13 00:45:27 +0300

    Register

--- registration/myapp/views.py
+++ registration/myapp/views.py
@@ -1,29 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.views import LoginView
-from django.views.generic.edit import CreateView
-from django.shortcuts import render, redirect
-import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth.models import User
-
-
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login
 
 class RegisterAPI(APIView):
     def post(self, request):
-        username = request.data.get("username")
-        email = request.data.get("email")
-        password = request.data.get("password")
-        if not username or not email or not password:
-            return Response({"detail": "Все поля обязательны"}, status=status.HTTP_400_BAD_REQUEST)
-        if User.objects.filter(username=username).exists():
-            return Response({"username": ["Пользователь с таким именем уже существует"]}, status=status.HTTP_400_BAD_REQUEST)
-        if User.objects.filter(email=email).exists():
-            return Response({"email": ["Пользователь с таким email уже существует"]}, status=status.HTTP_400_BAD_REQUEST)
-        user = User.objects.create_user(username=username, email=email, password=password)
-        return Response({"detail": "Пользователь успешно зарегистрирован"}, status=status.HTTP_201_CREATED)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"detail": "Пользователь успешно зарегистрирован"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class LoginAPI(APIView):
     def post(self, request):
         username = request.data.get("username")
