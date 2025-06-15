02b4bc7e121bebfedc4ac4e007f0bf6e82ea4b8c
Georgy451 <wiktory1975@gmail.com>
2025-06-16 02:40:20 +0300

    миграции

--- videosite/video/models.py
+++ videosite/video/models.py
@@ -12,6 +12,8 @@ class Moment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Связь с пользователем
     location = models.CharField(max_length=255, blank=True, null=True)
+    likes = models.PositiveIntegerField(default=0)  # Поле для хранения количества лайков
+    comments = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.created_at}"
